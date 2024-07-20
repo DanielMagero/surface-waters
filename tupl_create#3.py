9b93d45fe30c4c2173cb8cee8752a5ec21f6670c
DanielMagero <mdmagero700@gmail.com>
2024-07-20 12:09:34 +0300

    first

---
+++ tupl_create#3.py
@@ -0,0 +1,7 @@
+def tuple_make(pos):
+    num = (1, 3, 7, 9)
+    print(f"The number in postion {pos} is {num[pos]}")
+
+pos = int(input("Enter a postion of your choice to identify the number:"))
+
+tuple_make(pos)
---
+++ tupl_to_string#6.py
@@ -0,0 +1,7 @@
+def tupl2str(tupl1):
+    return ''.join(map(str, tupl1))
+
+
+tupl = ("a", "n", "t")
+
+print(f"The string made from the tuple is: {tupl2str(tupl)}")
